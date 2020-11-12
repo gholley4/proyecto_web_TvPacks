acab1b0a9064151596078384438312ad6df852ee
gholley4 <gholley42@gmail.com>
2020-11-12 14:02:27 -0300

    Creacion base de datos (clase cliente), creacion de super usuario en Django

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+        nombre = models.CharField(max_length=50)
+        rut = models.CharField(max_length=10)
+        email = models.CharField(max_length=50)
+        telefono = models.IntegerField()
+        region = models.CharField(max_length=30)
+        fecha_nacimiento = models.DateField()
+        tipo_pack = models.CharField(max_length=20)        
+
+        def __str__(self):
+            return self.nombre
